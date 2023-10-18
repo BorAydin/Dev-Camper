85aeaa9f69c981a91e3f0dd4db62ef1aae045207
BorAydin <boraaydins@gmail.com>
2023-10-18 09:49:38 +0300

    Creating Routes & Responses In Express

--- server.js
+++ server.js
@@ -6,6 +6,32 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ succces: true, msg: 'Tüm eğitimleri göster' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ succces: true, msg: `${req.params.id} idli eğitimi getir.` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ succces: true, msg: 'Yeni eğitim oluştur' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ succces: true, msg: `${req.params.id} idli eğitimi güncelle.` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ succces: true, msg: `${req.params.id} idli eğitimi sil.` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
