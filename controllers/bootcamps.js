b94b6c33f5a273e3d0d31804aa8fa3945c2f3464
BorAydin <boraaydins@gmail.com>
2023-10-19 17:47:39 +0300

    Create Bootcamp - POST

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // desc     Get all bootcamps
 // @route   GET api/v1/bootcamps
 // access   Public
@@ -17,8 +19,17 @@ exports.getBootcamp = (req, res, next) => {
 // desc     Create new bootcamp
 // @route   POST api/v1/bootcamps
 // access   Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ succces: true, msg: 'Yeni eğitim oluştur' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      succces: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({ sucess: false });
+  }
 };
 
 // desc     Update bootcamp
