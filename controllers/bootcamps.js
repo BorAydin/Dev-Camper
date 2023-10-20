1bceb759c1c945c14359c98de722327658aa47ed
BorAydin <boraaydins@gmail.com>
2023-10-20 09:01:34 +0300

    Fetchings Bootcamps - GET

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,17 +3,31 @@ const Bootcamp = require('../models/Bootcamp');
 // desc     Get all bootcamps
 // @route   GET api/v1/bootcamps
 // access   Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ succces: true, msg: 'Tüm eğitimleri göster' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ succces: true, data: bootcamps });
+  } catch (err) {
+    res.status(400).json({ succces: false });
+  }
 };
 
 // desc     Get single bootcamps
 // @route   GET api/v1/bootcamps/:id
 // access   Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ succces: true, msg: `${req.params.id} idli eğitimi getir.` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      res.status(400).json({ succces });
+    }
+
+    res.status(200).json({ succces: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ succces: false });
+  }
 };
 
 // desc     Create new bootcamp
