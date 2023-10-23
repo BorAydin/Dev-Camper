9653d825474aeab3a30d59f99378b8e7bb45ef00
BorAydin <boraaydins@gmail.com>
2023-10-23 09:13:13 +0300

    Async/Await Middleware

--- middleware/error.js
+++ middleware/error.js
@@ -20,11 +20,12 @@ const errorHandler = (err, reg, res, next) => {
     error = new ErrorResponse(message, 400);
   }
 
-  // Mongoose duplicate key
-  if (err.code === 11000) {
-    const message = 'Daha önce girilen bir değeri girdiniz.';
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.erros).map((val) => val.message);
     error = new ErrorResponse(message, 400);
   }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Sunucu Hatası',
