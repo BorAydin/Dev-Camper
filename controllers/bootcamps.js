e733668c693a38e7fd78c8b64949b861d01ac22e
BorAydin <boraaydins@gmail.com>
2023-10-24 10:42:48 +0300

    Select & Sorting

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,7 +13,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   const reqQuery = { ...req.query };
 
   // Fields to exclude
-  const removeFields = ['select'];
+  const removeFields = ['select', 'sort'];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach((param) => delete reqQuery[param]);
@@ -32,8 +32,16 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   // Select Fields
   if (req.query.select) {
-    const fields = req.query.select.split(',').join(' '); // İstekteki parametre alanları , ile ayrıldığı için önce diziye çevirip, boşluklu olacak şekilde String dönüşümünü join(' ') yapıyoruz. Boşluğun sebebi mongoose tarafı dökümantasyon query.select( 'name occupation' ) biçiminde çalışması. Araya, ortaya boşluk.
+    const fields = req.query.select.split(',').join(' ');
     query = query.select(fields);
+  }
+
+  // Sort Fields
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort('-createdAt');
   }
 
   // Executing query
