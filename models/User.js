a44a4f13dd368511d7bc892d05075a9431da947d
BorAydin <boraaydins@gmail.com>
2023-10-27 10:04:07 +0300

    User Login

--- models/User.js
+++ models/User.js
@@ -48,4 +48,9 @@ UserSchema.methods.getSignedJwtToken = function () {
   });
 };
 
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password); // return true or false
+};
+
 module.exports = mongoose.model('User', UserSchema);
